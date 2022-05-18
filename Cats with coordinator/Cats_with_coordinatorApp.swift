39ba3c6413c668e05566be2feadddf9e9d2c7c46
openmetrue <openmetrue@gmail.com>
2022-05-18 17:28:43 +0800

    Initial Commit

---
+++ Cats with coordinator/Cats_with_coordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cats_with_coordinatorApp.swift
+//  Cats with coordinator
+//
+//  Created by Mark Khmelnitskii on 18.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Cats_with_coordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
